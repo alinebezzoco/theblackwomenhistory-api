d97759a9d0bdeb6b5d369a1d9b749bf6c3254929
Aline Bezzoco <contato.alinebezzoco@gmail.com>
2018-04-29 23:02:40 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+fetch('https://api.cosmicjs.com/v1/blackwomenhistory/object/dandara?pretty=true&hide_metafields=true')
+    .then(response => response.json())
+    .then(data => {
+
+        document.getElementById("photo").src = data.object.metadata.image.url;
+        document.getElementById("name").innerHTML = data.object.title;
+        document.getElementById("description").innerHTML = data.object.content;
+        document.getElementById("credits").innerHTML = data.object.metadata.credits;
+        
+        
+    })
+    .catch(err => {
+        console.error('An error ocurred', err);
+    });
